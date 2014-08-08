cea61c18a9055d99ab494f0755f4fc6b3584c337
davidhurley <dhurls99@gmail.com>
2014-08-08 14:48:00 -0400

    cant add wine more then once

--- app/controllers/wracks_controller.rb
+++ app/controllers/wracks_controller.rb
@@ -3,14 +3,19 @@ class WracksController < ApplicationController
    def create
    	@wine = Wine.find(params[:wine_id])
   	@wrack = Wrack.new
+  	@racked_wines = Wrack.all
   	@wrack.user_id = current_user.id
   	@wrack.wine_id = @wine.id
 
-  	if @wrack.save
-  		redirect_to current_user, notice: 'Your wine is saved!'
+  	if @racked_wines.find_by(user_id: current_user.id, wine_id: @wine.id)
+  		redirect_to current_user, notice: 'Already picked that one.'
   	else
-  		render :new
-  	end
+	  	if @wrack.save
+	  		redirect_to current_user, notice: 'Your wine is saved!'
+	  	else
+	  		render :new
+	  	end
+	  end
   end
 
   private 
